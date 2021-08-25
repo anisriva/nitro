73f6ef143768563cc0f1f6862f4f4d2e7c0f5891
anisriva <animesh.srivastava@digicert.com>
2021-08-25 14:09:05 +0530

    minimized

--- final/.build/debezium-cassandra-init/startup-script.sh
+++ final/.build/debezium-cassandra-init/startup-script.sh
@@ -12,4 +12,4 @@ cqlsh -f $DEBEZIUM_HOME/inventory.cql
 java -Dlog4j.debug \
 -Dlog4j.configuration=file:$DEBEZIUM_HOME/log4j.properties \
 -jar $DEBEZIUM_HOME/debezium-connector-cassandra.jar \
-$DEBEZIUM_HOME/config.properties #>  $DEBEZIUM_HOME/debezium.stdout.log 2> $DEBEZIUM_HOME/debezium.stderr.log
+$DEBEZIUM_HOME/config.properties >  $DEBEZIUM_HOME/debezium.stdout.log 2> $DEBEZIUM_HOME/debezium.stderr.log
